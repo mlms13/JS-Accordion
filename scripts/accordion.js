e86c0443a6f0c7c3f485b2ebd0d7f8b436559094
Michael Martin-Smucker <mlmartin13@gmail.com>
2012-09-17 14:16:49 -0400

    Convert project to a jQuery plugin (Fixes #3)
    This commit converts the old accordion.js into a true jQuery plugin. While
    this changes the way that the accordion is initialized, not much else has
    changed at this point. We still use global variables rather than a
    settings object, and there are some other questionable decisions that have
    been carried over from the previous version. At least we're caching
    elements now.

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,78 +1,88 @@
-var visiblePanels = null; // each page can set this to control which sections are open by default (see html documentation)
+(function ($) {
+    $.fn.jsAccordion = function () {
+        var settings = {
+            'className' : 'secondary-nav',
+            'openPanels' : visiblePanels
+        };
 
-function Accordion() {
-    this.initialize = function() {
-        // add the style class to each accordion
-        $('.jsAccordion').addClass('secondary-nav');
-        // hide all sub-links
-        $('.jsAccordion > ul > li').children('ul, div').each(function () {
-            var padding = parseInt($(this).css('paddingLeft'), 10) + parseInt($(this).css('paddingRight'), 10),
-                border = parseInt($(this).css('borderLeft'), 10) + parseInt($(this).css('borderRight'), 10),
-                margin = parseInt($(this).css('marginLeft'), 10) + parseInt($(this).css('marginRight'), 10),
-                w = $(this).parent().width() - (padding + border + margin);
+        return this.each(function () {
+            var $this = $(this),
+                $li = $this.children('ul').children('li'),
+                showPanels;
 
-            $(this).hide().css({width: w});
-        });
-        
-        // find all non-empty, top-level text nodes and wrap them with span tags
-        $('.jsAccordion > ul > li').contents().filter(function() {
-            return this.nodeType === 3 && $.trim(this.nodeValue) !== '';
-        }).wrap('<span></span>');
+            // add the style class to each accordion
+            $this.addClass(settings.className);
 
-        // if an anchor precedes a hidden section, don't treat it as a link
-        $('.jsAccordion > ul > li > a').each(function() {
-            if ($(this).next('ul, div').length > 0) {
-                $(this).click(function(e) {
-                    e.preventDefault();
-                });
-            }
-        });
-        
-        $('.jsAccordion > ul > li > a, .jsAccordion > ul > li > span').click(function() {
-            if ($(this).next('ul, div').is(':visible')) {
-                // if you're closing a panel, don't hide all other panels
-                // (this lets you close one panel without closing all panels if multiple are open)
-                $(this).next('ul, div').slideUp();
-                $(this).parent().removeClass('expanded');
-            }
-            else {
-                // if you're opening a panel, it's safe to hide other panels in the same accordion
-                $(this).parents('.jsAccordion').find('.expanded').removeClass('expanded')
-                    .children('ul:visible, div:visible').slideUp();
-                $(this).next('ul, div').slideDown();
-                $(this).parent().addClass('expanded');
-            }
+            // hide all of the accordion's panels
+            $li.children('ul, div').each(function () {
+                // animation is smoother if the panel's width is set
+                // we calculate it automatically based on the parent's width
+                var $panel = $(this),
+                    padding = parseInt($panel.css('paddingLeft'), 10) + parseInt($panel.css('paddingRight'), 10),
+                    border = parseInt($panel.css('borderLeft'), 10) + parseInt($panel.css('borderRight'), 10),
+                    margin = parseInt($panel.css('marginLeft'), 10) + parseInt($panel.css('marginRight'), 10),
+                    w = $panel.parent().width() - (padding + border + margin);
+
+                $panel.hide().css({width: w});
+            });
+
+            // find all non-empty, top-level text nodes and wrap them with span tags
+            $li.contents().filter(function () {
+                return this.nodeType === 3 && $.trim(this.nodeValue) !== '';
+            }).wrap('<span />');
+
+            // if an anchor precedes a hidden section, don't treat it as a link
+            $li.children('a').each(function () {
+                var $a = $(this);
+
+                if ($a.next('ul, div').length > 0) {
+                    $a.click(function (e) {
+                        e.preventDefault();
+                    });
+                }
+            });
+
+            $li.children('a, span').click(function () {
+                var $label = $(this);
+
+                if ($label.next('ul, div').is(':visible')) {
+                    // if the corresponding panel is visible, collapse it
+                    $label.next('ul, div').slideUp();
+                    $label.parent().removeClass('expanded');
+                } else {
+                    // otherwise, if a panel is being opened, collapse all others
+                    $label.parents('.jsAccordion').find('.expanded').removeClass('expanded')
+                        .children('ul:visible, div:visible').slideUp();
+                    $label.next('ul, div').slideDown();
+                    $label.parent().addClass('expanded');
+                }
+            });
+
+            // determine which panels should be shown when the page loads
+            showPanels = function (panels) {
+                var i;
+
+                if (panels !== null) {
+                    if (panels === '-all') {
+                        $li.children('ul, div').show();
+                        $li.addClass('expanded');
+                    } else if (typeof panels === 'string') {
+                        // assume we're working with an id; remove hash if present
+                        if (panels.substr(0, 1) === '#') {
+                            panels = panels.substr(1, panels.length);
+                        }
+                        $this.find(panels).show().parent().addClass('expanded');
+                    } else if (typeof panels === 'number') {
+                        $li.children('ul, div').eq(panels).show().parent().addClass('expanded');
+                    } else if (panels.constructor === Array) {
+                        // loop through the array and handle each item individually
+                        for (i = 0; i < panels.length; i += 1) {
+                            showPanels(panels[i]);
+                        }
+                    }
+                }
+            };
+            showPanels(settings.openPanels);
         });
     };
-    // determine which panels should be shown when the page loads
-    this.showPanels = function(openPanels) {
-        var i = 0;
-        if (openPanels !== null) {
-            if (openPanels === '-all') {
-                $('.jsAccordion ul ul, .jsAccordion ul div').show();
-                $('.jsAccordion ul>li').addClass('expanded');
-            }
-            else if (typeof openPanels === 'string') {
-                // if there's a hash in front of the id, remove it
-                if (openPanels.substr(0, 1) === '#') {
-                    openPanels = openPanels.substr(1, openPanels.length);
-                }
-                $('.jsAccordion ul #' + openPanels).show().parent().addClass('expanded');
-            }
-            else if (typeof openPanels === 'number') {
-                $('.jsAccordion ul ul, .jsAccordion ul div').eq(openPanels).show().parent().addClass('expanded');
-            }
-            // if openPanels contains an array of items, loop through it and handle each one individually
-            else if (openPanels.constructor === Array) {
-                for (i = 0; i < openPanels.length; i++) {
-                    this.showPanels(openPanels[i]);
-                }
-            }
-        }
-    };
-}
-$(document).ready(function() {
-    var accordion = new Accordion();
-    accordion.initialize();
-    accordion.showPanels(visiblePanels);
-});
+}(jQuery));
