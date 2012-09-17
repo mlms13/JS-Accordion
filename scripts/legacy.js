2cdd670a3cf58a9cebc3dce550ef230c5a12ca57
Michael Martin-Smucker <mlmartin13@gmail.com>
2012-09-17 15:07:59 -0400

    Add a compatibility layer for legacy accordions (see #22)

---
+++ scripts/legacy.js
@@ -0,0 +1,7 @@
+// Name: Accordion Compatibility Layer
+// Goal: smoothly transition legacy accordions to the new jQuery plugin
+// Author: Michael Martin-Smucker
+
+$(function () {
+    $('.jsAccordion').jsAccordion();
+})
